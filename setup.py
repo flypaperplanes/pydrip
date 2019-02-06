357737a66d7870dcbbb3460b6ef0182ec53f67cc
Matthew Clarkson <matt@hilenium.com>
2019-02-06 15:20:41 +1000

    requirements

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
     setuptools.setup(
-     name='pydrip',  
-     version='0.1.0',
+     name='pydrip',
+     version='0.1.5',
      author="Matthew Clarkson",
      author_email="mpclarkson@gmail.com",
      description="A Python 3 client for the Drip API.",
      long_description=long_description,
      long_description_content_type="text/markdown",
      url="https://github.com/flypaperplanes/pydrip",
-     packages=setuptools.find_packages(),
+     packages=['drip'],
      classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
